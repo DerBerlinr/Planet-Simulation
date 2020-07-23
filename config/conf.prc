2ce31dec798d9c4aaf00eb1d88b01c15015bcd05
erikhaarlander <wertist.2000@gmail.com>
2020-07-23 13:03:00 +0200

    ein bisschen herumgespielt mit simpy und panda3d und mich da reingearbeitet

---
+++ config/conf.prc
@@ -0,0 +1,3 @@
+win-size 1280 720
+window-title My Game
+show-frame-rate-meter true
---
+++ objects.py
@@ -0,0 +1,13 @@
+class Planet:
+    def __init__(self):
+        pass
+
+
+class Comet:
+    def __init__(self):
+        pass
+
+
+class Moon:
+    def __init__(self):
+        pass
