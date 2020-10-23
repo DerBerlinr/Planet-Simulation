31da0f71fd88968595856371b7688f8632e1e550
eri47 <34981166+eri47@users.noreply.github.com>
2020-10-23 11:08:50 +0200

    added basic Planet Entity with keyboard movement

--- Ursina/main.py
+++ Ursina/main.py
@@ -1,11 +1,29 @@
 from ursina import *
 
-app = Ursina()
+app = Ursina() # App erstellen
 
-player = Entity(model='cube', color=color.orange, scale_y=2)
+window.title = 'Planetensimulation'  # 'Meta-Daten' der App setzen
+window.borderless = False
+window.fullscreen = False
+window.exit_button.visible = False
+window.fps_counter.enabled = True
 
-def update():   # update gets automatically called.
-    player.x += held_keys['d'] * .1
-    player.x -= held_keys['a'] * .1
+planets = []
 
+class Planet(Entity):
+    def __init__(self):
+        super().__init__(
+            model='sphere',
+            color=color.orange,
+        )
+        planets.append(self)
+    def update(self):   # update gets automatically called.
+        self.x += held_keys['d'] * .1
+        self.x -= held_keys['a'] * .1
+        self.y += held_keys['w'] * .1
+        self.y -= held_keys['s'] * .1
+        self.z += held_keys['q'] * .1
+        self.z -= held_keys['e'] * .1
+
+planet1 = Planet()
 app.run()   # opens a window and starts the game.
