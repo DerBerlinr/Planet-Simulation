e3eb0d00df879a37f390a15dba7cfea5042f5a98
eri47 <34981166+eri47@users.noreply.github.com>
2020-12-10 13:44:52 +0100

    Main.py formatted and added menu
    formatted and added basic menu, which opens by pressing 'esc', where you can reenter the game (working) and add planets (not working)

--- main.py
+++ main.py
@@ -1,6 +1,4 @@
 from ursina import *
-# from ursina.prefabs.first_person_controller import FirstPersonController
-from first_person import FirstPersonController
 
 app = Ursina()  # App erstellen
 
@@ -12,7 +10,8 @@ window.fps_counter.enabled = True
 
 
 class Planet(Entity):
-    def __init__(self, color, speed=1, mass=1, diameter=1, x=0, y=0, z=0):  # Entity wird erstellt, indem die init-Methode der Entity-Klasse aufgerufen wird
+    def __init__(self, color, speed=1, mass=1, diameter=1, x=0, y=0,
+                 z=0):  # Entity wird erstellt, indem die init-Methode der Entity-Klasse aufgerufen wird
         super().__init__(
             model='sphere',  # Planet soll ballförmig sein
             color=color,  # farbe des Planeten wird festgelegt
@@ -32,6 +31,9 @@ class Planet(Entity):
         self.y = y
         self.z = z
 
+# FIRST PERSON CONTROLLER +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 class FirstPersonController(Entity):
     def __init__(self, **kwargs):
         super().__init__()
@@ -41,45 +43,67 @@ class FirstPersonController(Entity):
         self.cursor = Entity(parent=camera.ui, model='quad', color=color.pink, scale=.008, rotation_z=45)
 
         camera.parent = self.camera_pivot
-        camera.position = (0,0,0)
-        camera.rotation = (0,0,0)
+        camera.position = (0, 0, 0)
+        camera.rotation = (0, 0, 0)
         camera.fov = 90
         mouse.locked = True
         self.mouse_sensitivity = Vec2(40, 40)
         self.target_smoothing = 100
         self.smoothing = self.target_smoothing
 
-button = Button(position=(-.5, .25, 0), text='button_text')
-button.scale *= .2
-button.text_entity.scale /= .2
-button.tooltip = Tooltip('tooltip text')
+    def update(self):
+        if mouse.locked:
+            for i in buttons:
+                i.enabled = False
+            # CAMERA ROTATION ------------------------------------------------------
+            self.rotation_y += mouse.velocity[0] * self.mouse_sensitivity[1]
 
+            self.camera_pivot.rotation_x -= mouse.velocity[1] * self.mouse_sensitivity[0]
+            self.camera_pivot.rotation_x = clamp(self.camera_pivot.rotation_x, -90, 90)
+
+            # CAMERA POSITION ------------------------------------------------------
+            self.direction = Vec3(self.forward * (held_keys['w'] - held_keys['s']) +
+                                 self.right * (held_keys['d'] - held_keys['a']) +
+                                 self.up * (held_keys['space'] - held_keys['shift'])
+                                 ).normalized()
+
+            self.position += self.direction * self.speed * time.dt
+
+        # EXIT FPC -----------------------------------------------------------------
+        if held_keys['escape']:
+            for i in buttons:
+                i.enabled = True
+            mouse.locked = False
+
+
+# BUTTONS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+buttons = []
+
+# BUTTON FUNCS ---------------------------------------------------------------------
+
+def reenter_game():
+    mouse.locked = True
+
+
+def add_Planet():
+    for i in buttons:
+        i.enabled = False
+
+# BUTTONS --------------------------------------------------------------------------
+
+bu_reenter = Button(position=(0, .25, 0), text='Reenter Game', scale=(.5, .07))
+buttons.append(bu_reenter)
+bu_reenter.on_click = reenter_game
+
+bu_add = Button(position=(0, .15, 0), text='Add Planet', scale=(.5, .07))
+buttons.append(bu_add)
+bu_add.on_click = add_Planet
+
+# SETUP +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 planet = Planet(color=color.orange)  # crate a planet
-planet.set_coords(9,0,9)  # set Pos of planet
+planet.set_coords(9, 0, 9)  # set Pos of planet
 
-cam = FirstPersonController() # FPC wird erstellt
-
-def update_camera():
-    # CAMERA ROTATION ---------------------------------------------------------------
-    cam.rotation_y += mouse.velocity[0] * cam.mouse_sensitivity[1]
-
-    cam.camera_pivot.rotation_x -= mouse.velocity[1] * cam.mouse_sensitivity[0]
-    cam.camera_pivot.rotation_x = clamp(cam.camera_pivot.rotation_x, -90, 90)
-
-    # CAMERA POSITION ---------------------------------------------------------------
-    cam.direction = Vec3(cam.forward * (held_keys['w'] - held_keys['s']) +
-                          cam.right * (held_keys['d'] - held_keys['a']) +
-                          cam.up * (held_keys['space'] - held_keys['shift'])
-                          ).normalized()
-
-    cam.position += cam.direction * cam.speed * time.dt
-
-    # EXIT FPC ---------------------------------------------------------------------
-
-    if held_keys['escape']:
-        mouse.locked = False
-
-cam.update = update_camera
+fpc = FirstPersonController()
 
 app.run()
