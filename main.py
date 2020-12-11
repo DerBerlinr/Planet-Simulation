3ad35ebe681453adeb985192d2140adb5858777e
eri47 <34981166+eri47@users.noreply.github.com>
2020-12-11 15:31:41 +0100

    Added form for adding planets [WIP]

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from ursina import *
 
-app = Ursina()  # App erstellen
+app = Ursina()
 
-window.title = 'Planetensimulation'  # 'Meta-Daten' der App setzen
+window.title = 'planet simulation'  # set meta data for app
 window.borderless = True
 window.fullscreen = True
 window.exit_button.visible = True
@@ -10,15 +10,15 @@ window.fps_counter.enabled = True
 
 
 class Planet(Entity):
-    def __init__(self, color, speed=1, mass=1, diameter=1, x=0, y=0,
-                 z=0):  # Entity wird erstellt, indem die init-Methode der Entity-Klasse aufgerufen wird
+    def __init__(self, color, name, speed=1, mass=1, diameter=1, x=0, y=0, z=0):
         super().__init__(
-            model='sphere',  # Planet soll ballförmig sein
-            color=color,  # farbe des Planeten wird festgelegt
+            model='sphere',
+            color=color,
             collision=True,
             collider='sphere'
             # TODO: add Textures
         )
+        self.name = name
         self.speed = speed
         self.mass = mass
         self.scale = Vec3(diameter, diameter, diameter)
@@ -26,10 +26,11 @@ class Planet(Entity):
         self.y = y
         self.z = z
 
-    def set_coords(self, x, y, z):  # Koordinaten eines Planeten setzen
+    def set_coords(self, x, y, z):  # set coordinates of planet
         self.x = x
         self.y = y
         self.z = z
+
 
 # FIRST PERSON CONTROLLER +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
@@ -63,9 +64,9 @@ class FirstPersonController(Entity):
 
             # CAMERA POSITION ------------------------------------------------------
             self.direction = Vec3(self.forward * (held_keys['w'] - held_keys['s']) +
-                                 self.right * (held_keys['d'] - held_keys['a']) +
-                                 self.up * (held_keys['space'] - held_keys['shift'])
-                                 ).normalized()
+                                  self.right * (held_keys['d'] - held_keys['a']) +
+                                  self.up * (held_keys['space'] - held_keys['shift'])
+                                  ).normalized()
 
             self.position += self.direction * self.speed * time.dt
 
@@ -79,15 +80,69 @@ class FirstPersonController(Entity):
 # BUTTONS +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 buttons = []
 
+
 # BUTTON FUNCS ---------------------------------------------------------------------
 
 def reenter_game():
     mouse.locked = True
 
 
-def add_Planet():
+planet_data_temp = []
+
+
+def add_planet():
     for i in buttons:
         i.enabled = False
+
+    name_field = InputField(name='name_field')
+    mass_field = InputField(name='mass_field')
+    speed_field = InputField(name='speed_field')
+    pos_x_field = InputField(name='pos_x_field')
+    pos_y_field = InputField(name='pos_y_field')
+    pos_z_field = InputField(name='pos_z_field')
+
+    planet_data_temp.append(name_field)
+    planet_data_temp.append(mass_field)
+    planet_data_temp.append(speed_field)
+    planet_data_temp.append(pos_x_field)
+    planet_data_temp.append(pos_y_field)
+    planet_data_temp.append(pos_z_field)
+
+    win = WindowPanel(
+        title='add planet',
+        content=(
+            Text('name:'),
+            name_field,
+            Text('mass in kg:'),
+            mass_field,
+            Text('speed in m/s:'),
+            speed_field,
+            Text('x-position:'),
+            pos_x_field,
+            Text('y-position:'),
+            pos_y_field,
+            Text('z-position:'),
+            pos_z_field,
+            Button(text='Submit', color=color.azure, on_click=submit_planet_data)
+        ),
+    )
+    planet_data_temp.append(win)
+
+
+def submit_planet_data():
+    '''
+    name = planet_data_temp[0].text
+    mass = int(planet_data_temp[1].text)
+    speed = int(planet_data_temp[2].text)
+    pos_x = int(planet_data_temp[3].text)
+    pos_y = int(planet_data_temp[4].text)
+    pos_z = int(planet_data_temp[5].text)
+    temp = Planet(color=color.blue, name=name, speed=speed, mass=mass, x=pos_x, y=pos_y, z=pos_z)
+    planet_data_temp[6].close()
+    mouse.locked = True
+    '''
+    exit()
+
 
 # BUTTONS --------------------------------------------------------------------------
 
@@ -97,11 +152,11 @@ bu_reenter.on_click = reenter_game
 
 bu_add = Button(position=(0, .15, 0), text='Add Planet', scale=(.5, .07))
 buttons.append(bu_add)
-bu_add.on_click = add_Planet
+bu_add.on_click = add_planet
 
 # SETUP +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-planet = Planet(color=color.orange)  # crate a planet
+planet = Planet(color=color.orange, name="planet1")  # crate a planet
 planet.set_coords(9, 0, 9)  # set Pos of planet
 
 fpc = FirstPersonController()
